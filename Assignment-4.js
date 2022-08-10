7589ea089c6b5dd5eea14e062da70981636ced0f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-08-10 21:57:04 +0600

    problem-3 solved

--- Assignment-4.js
+++ Assignment-4.js
@@ -27,9 +27,32 @@ disel----> 114
 patrol---> 130
 octane----> 135
 */
-function oilPrice() {
+function oilPrice(diselQuantity, patrolQuantity, octaneQuantity) {
+    let diselPrice = 114;
+    let patrolPrice = 130;
+    let octanePrice = 135;
+
+    // Disel 
+    const totalDiselPrice = diselPrice * diselQuantity;
+    const disel = totalDiselPrice;
+    console.log(disel);
+
+    // Patrol
+    const totalPatrolPrice = patrolPrice * patrolQuantity;
+    const patrol = totalPatrolPrice;
+    console.log(patrol);
+
+    // Octan
+    const totalOctanePrice = octanePrice * octaneQuantity;
+    const octane = totalOctanePrice;
+    console.log(octane);
+
+    //Total price
+    const totalOilPrice = patrol + disel + octane;
+    console.log(totalOilPrice);
 
 }
+oilPrice(2, 4, 1);
 
 //4.
 /*
