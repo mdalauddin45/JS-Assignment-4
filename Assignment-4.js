b9d2515649198e86618d4addaf2fd83b719e1e09
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-08-10 20:51:12 +0600

    first commit

---
+++ Assignment-4.js
@@ -0,0 +1,49 @@
+//1. Problem-01 : radian to degree
+
+function radianToDegree(radian) {
+
+}
+
+// 2. check whether the given file name is a javaScript file or not
+function isJavaScriptFile(string) {
+
+}
+
+//3. calculate the total oil price that I have to pay
+/*
+disel----> 114
+patrol---> 130
+octane----> 135
+*/
+function oilPrice() {
+
+}
+
+//4.
+/*
+reserved bus ---> 50
+micro bus ------> 11
+rest prople will go by public bus
+people : 65
+bus remaining---> 15
+microbus -------> 4
+public bus------> 4
+public bus fare-> 250
+*/
+function publicBusFare(people) {
+
+}
+
+// 5.
+/*
+{name: 'tom', friend: 'Roke'}
+{name: "Roke", friend: "Tom"}
+
+
+{name: 'Chris', friend: 'John'}
+{name: "Raz", friend: "Jonny"}
+*/
+function isBestFriend() {
+
+}
+
